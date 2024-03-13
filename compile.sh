91d482af03949994e430c824a8c725eb65291b8e
Yotam Lifschytz <80533728+yotam-lif@users.noreply.github.com>
2024-03-13 20:09:44 +0200

    bash compile files try #1

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-bsub -q short -m cn1[12-38] -oo compile_out.txt -eo compile_err_out.txt "ml restore Evo && make"
+bsub -q short -m cn1[12-38] -oo compile_out.txt -eo compile_err_out.txt compile_tasks.sh
---
+++ compile_tasks.sh
@@ -0,0 +1,2 @@
+ml restore Evo
+make
