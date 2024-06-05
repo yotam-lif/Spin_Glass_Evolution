7e4a6915b0fde08db46b8d81e2bc4e1a5a0d733c
Yotam Lifschytz <80533728+yotam-lif@users.noreply.github.com>
2024-06-05 14:23:41 +0300

    1. makefile with clean for compile files 2. Added working compile script

--- compile.sh
+++ compile.sh
@@ -1,1 +1,12 @@
-bsub -q short -m cn1[12-26] -oo compile_out.txt -eo compile_err_out.txt make
+# Load necessary modules
+module load Boost/1.82.0-GCC-12.3.0
+module load GSL/2.7-GCC-12.3.0
+
+# Export environment variables
+export BOOST_INCLUDE=/apps/easybd/easybuild/software/Boost/1.82.0-GCC-12.3.0/include
+export BOOST_LIB=/apps/easybd/easybuild/software/Boost/1.82.0-GCC-12.3.0/lib
+export GSL_INCLUDE=/apps/easybd/easybuild/software/GSL/2.7-GCC-12.3.0/include
+export GSL_LIB=/apps/easybd/easybuild/software/GSL/2.7-GCC-12.3.0/lib
+
+# Run the make command
+make
